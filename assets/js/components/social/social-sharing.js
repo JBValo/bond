ab4aadcd7a4770a0fb72b62f67c6b15ed88bfcf5
Justin Bellefontaine <justin@norex.ca>
2018-06-29 14:13:37 -0300

    updating content for demo page, adding email sharing scaffolding

--- assets/js/components/social/social-sharing.js
+++ assets/js/components/social/social-sharing.js
@@ -12,9 +12,15 @@
 
     $(document).ready(function() {
 
+        // Show and Hide Options
         $('.social-sharing__trigger').on('click', function(e) {
             $(this).hide();
             $(this).siblings('.social-sharing__list').addClass('show');
+        });
+
+        // Email Option
+        $('.social-sharing__list-item-link--email').on('click', function(e) {
+            $('.social-sharing__email-form').addClass('show');
         });
 
     });
