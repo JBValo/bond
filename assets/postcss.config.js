28a16731a4f8582466e8688c6e12e789b5641af1
Justin Bellefontaine <justin@norex.ca>
2018-07-24 09:59:59 -0300

    removing critical css for now

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -9,13 +9,13 @@ module.exports = {
     ]
 };
 
-var critical = require('critical');
-
-critical.generate({
-    base: '../',
-    src: 'index.html',
-    dest: 'assets/css/style.min.css',
-    minify: true,
-    width: 1300,
-    height: 900
-});
+// var critical = require('critical');
+//
+// critical.generate({
+//     base: '../',
+//     src: 'index.html',
+//     dest: 'assets/css/style.min.css',
+//     minify: true,
+//     width: 1300,
+//     height: 900
+// });
