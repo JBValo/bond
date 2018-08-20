d2c102703b9f1ac74c37dfd5c4826f0d81bdbb3e
Justin Bellefontaine <justin@norex.ca>
2018-08-20 09:39:45 -0300

    fixing grid syntax

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -3,19 +3,15 @@ module.exports = {
         require('postcss-import')({}),
         require('postcss-mixins')({}),
         require('postcss-nested')({}),
+        require('autoprefixer')({
+            browsers: '> 2.5% in CZ',
+            flexbox: 'no-2009'}
+        ),
         require('postcss-simple-vars')({}),
         require('postcss-color-mod-function')({}),
         require('postcss-responsive-type')({}),
+        require('cssnano')({
+            autoprefixer: false
+        })
     ]
 };
-
-// var critical = require('critical');
-//
-// critical.generate({
-//     base: '../',
-//     src: 'index.html',
-//     dest: 'assets/css/style.min.css',
-//     minify: true,
-//     width: 1300,
-//     height: 900
-// });
